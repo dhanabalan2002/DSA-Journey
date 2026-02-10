7944ae4cc34ef4c4e9a252e40054059889ef764e
dhanabalan <dhanabalan.muniyandi@aroopatech.com>
2026-02-10 19:46:28 +0530

    Two Sum

---
+++ Arrays/two-sum.js
@@ -0,0 +1,25 @@
+// Two Sum
+// input: nums = [2, 8, 3, 9, 7]
+// target: 11
+// output = [0,4]
+
+// pseudo code:
+// for i = 0; i < nums.length; i++
+// for j = 0; j < nums.length; j++
+// answer = nums[i] + nums[j]
+// if answer == target
+// return [i, j]
+
+const numbers = [2, 8, 3, 9, 7];
+const target = 9;
+for (i = 0; i < numbers.length; i++) {
+    for (j = 0; j < numbers.length; j++) {
+        let answer = numbers[i] + numbers[j];
+        if (answer === target) {
+            console.log([i, j]);
+            return [i, j];
+        }
+    }
+}
+
+// j = i + 1; best case
